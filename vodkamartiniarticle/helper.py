9136a6ea3c2cdc3bf22c64646922524290b758d3
Alexis Bellido <alexis@ventanazul.com>
2013-02-20 14:59:33 -0500

    check internal spam words and get client ip for article

--- vodkamartiniarticle/helper.py
+++ vodkamartiniarticle/helper.py
@@ -1,6 +1,17 @@
 import re
 from django.template.defaultfilters import slugify
+from django.conf import settings
 
+def check_internal_spam_words(content):
+    return any(word in (content) for word in settings.SPAM_WORDS)
+
+def get_client_ip(request):
+    x_forwarded_for = request.META.get('HTTP_X_FORWARDED_FOR')
+    if x_forwarded_for:
+        ip = x_forwarded_for.split(',')[0]
+    else:
+        ip = request.META.get('REMOTE_ADDR')
+    return ip
 
 def unique_slugify(instance, value, slug_field_name='slug', queryset=None,
                    slug_separator='-'):
